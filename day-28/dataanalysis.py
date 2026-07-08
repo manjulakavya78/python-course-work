38b11ca55e7637a4b928d6e744e8a7c51ba434a5
KAVYA MANJULA <manjulakavya78@gmail.com>
2026-07-08 11:02:41 +0530

    Delete day-28 directory

--- day-28/dataanalysis.py
+++
@@ -1,5 +0,0 @@
-import re
-pattern = r'^[A-Za-z]{2,15} ( [A-Za-z]{2,15})+$'
-name = input("Enter the name:")
-res = re.fullmatch(pattern,name)
-print("Valid name" if res else "Invalid name")
--- day-28/emailvalidation.py
+++
@@ -1,5 +0,0 @@
-import re
-pattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-email = input("Enter the email:")
-res = re.fullmatch(pattern,email)
-print("Valid email" if res else "Invalid email")
